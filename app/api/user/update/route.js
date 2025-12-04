dd89db2bf562cbe5a12dfc8bafefac1fcbb53980
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-12-04 16:02:44 +0100

    Trying to fix profilecompletion

--- app/api/user/update/route.js
+++ app/api/user/update/route.js
@@ -2,14 +2,12 @@ import User from "@/models/User";
 import { connectDB } from "@/lib/db";
 import { profileCompletion } from "@/lib/profileCompletion";
 
-
 export async function PUT(req) {
   await connectDB();
   const { userId, updates } = await req.json();
 
   const user = await User.findByIdAndUpdate(userId, updates, { new: true });
 
-  // ✅ Recalculate completion
   user.profileCompletionPercent = profileCompletion(user);
   await user.save();
 
