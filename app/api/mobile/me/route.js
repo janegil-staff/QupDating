9f4168aca91adda9e1bc4a23460497409425a6c2
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-12-09 00:17:40 +0100

    Fixed a wrong import

--- app/api/mobile/me/route.js
+++ app/api/mobile/me/route.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
-import connectDB from "@/lib/mongodb";
 import User from "@/models/User";
+import { connectDB } from "@/lib/db";
 
 export async function GET(req) {
   const authHeader = req.headers.get("authorization");
